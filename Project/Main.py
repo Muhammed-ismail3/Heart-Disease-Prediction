562a6e36882a5be7ee04464c3675f8c5a6a1b072
Dark_Knight <muhammed.ismail3tr@gmail.com>
2025-11-03 20:25:36 +0300

    i cleaned the data, added the cleanded file

--- Project/Main.py
+++ Project/Main.py
@@ -31,7 +31,7 @@ data = pd.read_csv("heart.csv")
 
 # print(data["ChestPain"].unique())
 
-print(data[data["Chol"] > 360])
+# print(data[data["Chol"] > 360])
 # print(pd.crosstab(data["RestBP"] > 170,data["Target"]))
 
 # print(pd.crosstab(data["Target"], data["Sex"]).loc[1].sum())
@@ -43,14 +43,34 @@ print(data[data["Chol"] > 360])
 
 # plt.boxplot(data["Chol"])
 
-# data = data.dropna()
-# print(data.isnull().sum())
+data = data.dropna()
+data.isnull().sum()
 
 # data[["Sex","Target"]].hist()
 
 # data.plot( x='Sex', y='Target', kind='hist')
 
 # data["RestBP"].hist()
+
+data2 = data.drop(152, axis= 0)
+
+# print(data[data["Chol"] > 360])
+
+# print("\n-----------------------\n")
+# print(data2[data2["Chol"] > 360])
+# print("\n-----------------------\n")
+
+# print(data2[data2["RestBP"]> 170])
+# print(data2.isnull().sum())
+# data2[["MaxHR","Oldpeak"]].hist(edgecolor = 'black')
+
+# print(len(data2[(data2["Oldpeak"] > 2)]))
+
+print(len(data2[data2["Oldpeak"]>= 4]))
+
+# plt.boxplot(data2["Oldpeak"])
+
+data2.to_csv("Data_Cleaned.csv")
 
 
 plt.show()
